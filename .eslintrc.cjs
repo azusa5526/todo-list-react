04778f37bfdffc8a3881eb05faeb1137a5a2684e
AndrewYao <andrewyao@anasystem.com.tw>
2024-08-19 10:46:40 +0800

    chore: 修改 eslint 規則

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,5 +13,6 @@ module.exports = {
   rules: {
     'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
     '@typescript-eslint/no-unused-vars': 'warn',
+    '@typescript-eslint/ban-ts-comment': 0,
   },
 };
