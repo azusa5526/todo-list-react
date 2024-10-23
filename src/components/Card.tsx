45695e4d5f6e9f2b8e7c57d93b94c81fd0d02874
AndrewYao <andrewyao@anasystem.com.tw>
2024-10-23 17:29:44 +0800

    feat(WIP): 製作 card dialog

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,19 +1,61 @@
 import type { Card } from '../api/trello-type';
 import { getRandomColor } from '@/utils/getRandomColor';
+import { Description, Dialog, DialogPanel, DialogTitle } from '@headlessui/react';
+import { useState } from 'react';
 
 export default function Card({ card }: { card: Card }) {
+  const apiUrl = import.meta.env.VITE_API_URL;
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const openDialog = () => setIsDialogOpen(true);
+  const closeDialog = () => setIsDialogOpen(false);
+
   return (
-    <div className='card rounded-md py-12 text-white' style={{ backgroundColor: getRandomColor() }}>
-      <h3 className='text-lg font-bold'>{card.title}</h3>
+    <div
+      className='card overflow-hidden rounded-lg text-white'
+      style={{ backgroundColor: getRandomColor() }}
+      onClick={openDialog}
+    >
+      {card.coverImage && (
+        <img
+          src={`${apiUrl}/${card.coverImage}`}
+          alt={card.title}
+          className='h-auto w-full object-cover'
+          draggable='false'
+        />
+      )}
+      <h3 className='px-3 py-2 text-sm font-bold'>{card.title}</h3>
       <p className='text-sm'>{card.description}</p>
       {/* <p className='text-xs text-gray-400'>{new Date(card.date).toLocaleDateString()}</p> */}
-      {/* {card.coverImage && (
-        <img
-          src={card.coverImage}
-          alt={card.title}
-          className='mt-2 h-32 w-full rounded object-cover'
-        />
-      )} */}
+      <CardDialog card={card} isOpen={isDialogOpen} onClose={closeDialog} />
     </div>
+  );
+}
+
+interface CardDialogProps {
+  card: Card;
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+function CardDialog({ card, isOpen, onClose }: CardDialogProps) {
+  const apiUrl = import.meta.env.VITE_API_URL;
+
+  return (
+    <Dialog open={isOpen} onClose={onClose} className='relative z-50'>
+      <div className='fixed inset-0 flex w-screen items-center justify-center p-4'>
+        <DialogPanel className='max-w-lg space-y-4 border bg-white p-12'>
+          <DialogTitle className='font-bold'>Deactivate account</DialogTitle>
+          <Description>This will permanently deactivate your account</Description>
+          <p>
+            Are you sure you want to deactivate your account? All of your data will be permanently
+            removed.
+          </p>
+          <div className='flex gap-4'>
+            <button onClick={onClose}>Cancel</button>
+            <button onClick={onClose}>Deactivate</button>
+          </div>
+        </DialogPanel>
+      </div>
+    </Dialog>
   );
 }
