c8cf3f595a23754e1c2c213ed628754e104f455c
AndrewYao <andrewyao@anasystem.com.tw>
2024-10-26 19:21:49 +0800

    feat: 加入字體設定

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      fontFamily: { roboto: ['Roboto'], 'noto-sans-tc': ['Noto Sans TC'] },
       height: {
         appbar: 'var(--appbar-height)',
         boardbar: 'var(--boardbar-height)',
