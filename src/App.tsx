6c85b88d08bc5f2e12155838b86a43fd18e74558
DESKTOP-FIJNG4G\Andrew <azusa5526@gmail.com>
2024-10-26 21:50:53 +0800

    refactor: 抽離 boardSidebar

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,11 @@ import { getContainers } from '@/api/trello';
 import type { Container as ContainerType } from './api/trello-type';
 import Container from './components/Container';
 import { NewContainerButton } from './components/NewContainerButton';
+import { BoardSidebar } from './components/BoardSidebar';
 
 export function App() {
   const [containers, setContainers] = useState<ContainerType[]>([]);
-  const [sidebarOpen, setSidebarOpen] = useState(true);
+  const [boardSidebarOpen, setBoardSidebarOpen] = useState(true);
 
   useEffect(() => {
     fetchContainers();
@@ -22,13 +23,6 @@ export function App() {
     }
   };
 
-  const getRandomColor = () => {
-    const r = Math.floor(Math.random() * 256);
-    const g = Math.floor(Math.random() * 256);
-    const b = Math.floor(Math.random() * 256);
-    return `rgba(${r}, ${g}, ${b})`;
-  };
-
   function onAddContainer(container: ContainerType) {
     setContainers([...containers, container]);
   }
@@ -37,29 +31,25 @@ export function App() {
     setContainers(containers.filter((container) => container._id !== id));
   }
 
+  function onBoardSidebarToggle(isOpen: boolean) {
+    setBoardSidebarOpen(isOpen);
+  }
+
   return (
     <>
       <div className='flex h-appbar shrink-0 items-center bg-green-200'>
         <h1 className='text-xl'>吹囉</h1>
       </div>
       <div className='relative flex'>
-        <div className={`shrink-0 transition-[width] ${sidebarOpen ? 'w-72' : 'w-0'}`}></div>
-        <div
-          className={`absolute bottom-0 left-0 top-0 flex w-72 shrink-0 flex-col bg-red-400 transition-transform ${sidebarOpen ? 'translate-x-0' : '-translate-x-72'}`}
-        >
-          <div className='flex h-boardbar shrink-0 items-center'>你的看板</div>
-          <div className='custom-scrollbar overflow-y-auto'>
-            {Array.from({ length: 20 }, (_, index) => index++).map((item) => (
-              <div className='py-4' style={{ backgroundColor: getRandomColor() }} key={item}>
-                {item}
-              </div>
-            ))}
-          </div>
-        </div>
+        <BoardSidebar isOpen={boardSidebarOpen} onToggle={onBoardSidebarToggle}></BoardSidebar>
         <div className='flex grow flex-col overflow-x-auto bg-sky-700'>
           <div className='flex h-boardbar items-center bg-yellow-200'>
-            <div role='button' className='mr-4' onClick={() => setSidebarOpen(!sidebarOpen)}>
-              {sidebarOpen ? 'close' : 'open'}
+            <div
+              role='button'
+              className='mr-4'
+              onClick={() => setBoardSidebarOpen(!boardSidebarOpen)}
+            >
+              {boardSidebarOpen ? 'close' : 'open'}
             </div>
             <div>假的吹囉看板</div>
           </div>
